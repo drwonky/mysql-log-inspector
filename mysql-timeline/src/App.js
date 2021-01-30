30f1bb177de20abd6fdc34365acf421437329a09
Perry Harrington <pedward@apsoft.com>
2021-01-30 00:43:11 -0800

    Implemented log parser that produces JSON for log entries

--- mysql-timeline/src/App.js
+++ mysql-timeline/src/App.js
@@ -6,6 +6,68 @@ import { Folder, Play } from 'react-feather';
 
 const Context = React.createContext();
 
+/*
+5.6:
+enum loglevel {
+ ERROR_LEVEL=       0,
+ WARNING_LEVEL=     1,
+ INFORMATION_LEVEL= 2
+};
+
+(level == ERROR_LEVEL   ? "ERROR" : 
+ level == WARNING_LEVEL ? "Warning" : 
+                          "Note"),
+
+5.7:
+enum loglevel {
+ ERROR_LEVEL=       0,
+ WARNING_LEVEL=     1,
+ INFORMATION_LEVEL= 2
+};
+
+(level == ERROR_LEVEL   ? "ERROR" : 
+ level == WARNING_LEVEL ? "Warning" : 
+                          "Note"),
+
+8:
+enum loglevel {
+SYSTEM_LEVEL = 0,
+ERROR_LEVEL = 1,
+WARNING_LEVEL = 2,
+INFORMATION_LEVEL = 3
+};
+
+if (!(out_types & LOG_ITEM_LOG_LABEL)) {
+  label = (prio == ERROR_LEVEL) ? "ERROR" : log_label_from_prio(prio);
+  label_len = strlen(label);
+}
+
+@retval  "ERROR"    for prio of ERROR_LEVEL or higher
+@retval  "Warning"  for prio of WARNING_LEVEL
+@retval  "Note"     otherwise
+
+@retval  "System"   for prio of SYSTEM_LEVEL
+@retval  "Error"    for prio of ERROR_LEVEL
+@retval  "Warning"  for prio of WARNING_LEVEL
+@retval  "Note"     for prio of INFORMATION_LEVEL
+const char *log_label_from_prio(int prio)
+
+*/
+
+const loglevel = {
+  SYSTEM_LEVEL: 0,
+  ERROR_LEVEL: 1,
+  WARNING_LEVEL: 2,
+  INFORMATION_LEVEL: 3
+}
+
+const loglevel_strings = {
+  'System': loglevel.SYSTEM_LEVEL,
+  'ERROR': loglevel.ERROR_LEVEL,
+  'Warning': loglevel.WARNING_LEVEL,
+  'Note': loglevel.INFORMATION_LEVEL
+}
+
 export default class MySQLTimeline extends React.Component {
   constructor(props) {
     super(props)
@@ -51,52 +113,58 @@ class Uploader extends React.Component {
     }
   }
 
+  parseLogEntry(ver, line) {
+    return {
+      date: new Date(line[1]),
+      loglevel: loglevel_strings[line[3]],
+      subsystem: ver === '8' ? line[5] : null,
+      errcode: ver === '8' ? line[4].split('-')[1] : null,
+      msg: ver === '8' ? line[6] : line[4],
+    }
+  }
+
   // 5.6: 2014-07-25 16:13:10 0 [Warning] TIMESTAMP with implicit DEFAULT value is deprecated. Please use --explicit_defaults_for_timestamp server option (see documentation for more details).
   // 5.7: 2020-02-12T20:23:20.550920Z 0 [Warning] TIMESTAMP with implicit DEFAULT value is deprecated. Please use --explicit_defaults_for_timestamp server option (see documentation for more details).
   //   8: 2021-01-19T19:08:08.014889Z 0 [Warning] [MY-011070] [Server] 'Disabling symbolic links using --skip-symbolic-links (or equivalent) is the default. Consider not using this option as it' is deprecated and will be removed in a future release.
   processLogLines(text) {
-      let lines = text.split(/\r\n|\n/);
-      let regex_5_6 = /([0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+) ([0-9]+) (\[.*\]) (.*$)/
-      let regex_5_7 = /([0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.[0-9]+Z?) ([0-9]+) (\[.*\]) (.*$)/
-      let regex_8 = /([0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.[0-9]+Z?) ([0-9]+) (\[.*\]) (\[.*\]) (\[.*\]) (.*$)/
-      let regex = null;
-      let logdata = {};
+    let lines = text.split(/\r\n|\n/);
+    let regex_5_6 = /([0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+) ([0-9]+) \[(.*)\] (.*$)/
+    let regex_5_7 = /([0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.[0-9]+Z?) ([0-9]+) \[(.*)\] (.*$)/
+    let regex_8 = /([0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.[0-9]+Z?) ([0-9]+) \[(.*)\] \[(.*)\] \[(.*)\] (.*$)/
+    let regex = null;
+    let logdata = {};
 
-      console.log('lines.length',lines.length,lines[0]);
-      // the split gives us a null last line
-      if (lines.length > 0 && lines[0] === null) return null;
+    // the split gives us a null last line
+    if (lines.length > 0 && lines[0] === null) return null;
 
-      // test different regexes to figure out the right format
-      console.log('lines[0]',lines[0])
-      const test_5_6 = lines[0].match(regex_5_6);
-      const test_5_7 = lines[0].match(regex_5_7);
-      const test_8 = lines[0].match(regex_8);
-      console.log(test_5_6, test_5_7, test_8);
-      if (test_5_6 && test_5_6.length > 0) {
-        regex = regex_5_6;
-        logdata.version = '5.6';
-      } else if (test_8 && test_8.length > 0) {
-        regex = regex_8;
-        logdata.version = '8';
-      } else if (test_5_7 && test_5_7.length > 0) {
-        regex = regex_5_7;
-        logdata.version = '5.7';
-      } else {
-        return null;
+    // test different regexes to figure out the right format
+    const test_5_6 = lines[0].match(regex_5_6);
+    const test_5_7 = lines[0].match(regex_5_7);
+    const test_8 = lines[0].match(regex_8);
+    console.log(test_5_6, test_5_7, test_8);
+    if (test_5_6 && test_5_6.length > 0) {
+      regex = regex_5_6;
+      logdata.version = '5.6';
+    } else if (test_8 && test_8.length > 0) {
+      regex = regex_8;
+      logdata.version = '8';
+    } else if (test_5_7 && test_5_7.length > 0) {
+      regex = regex_5_7;
+      logdata.version = '5.7';
+    } else {
+      return null;
+    }
+
+    logdata.content = lines.reduce((result, entry) => {
+      let line = entry.match(regex);
+      if (line !== null) {
+        result.push(this.parseLogEntry(logdata.version, line));
       }
-
-      console.log('logdata',logdata);
-      logdata.content = lines.reduce((result, entry) => {
-        let line = entry.match(regex);
-        if (line !== null) {
-          let date = new Date(line[1]);
-          result.push([date, ...line.slice(1)]);
-        }
-        return result;
-      }, []);
+      return result;
+    }, []);
 
 
-      return logdata;
+    return logdata;
   }
 
   async handleFileChange(e) {
